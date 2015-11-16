77f5294de4ed2db37ef711f31229afef86f97d75
Peter Radcliffe <peter.radcliffe@cloudsherpas.com>
2015-11-16 19:46:38 +0000

    Added angular material library. Some dev around card hrefs

--- bupa-angular/app.js
+++ bupa-angular/app.js
@@ -1,4 +1,4 @@
-angular.module('bupasearch', ['ngRoute', 'buparesults'])
+angular.module('bupasearch', ['ngRoute', 'buparesults', 'ui.materialize'])
 .config(['$routeProvider', '$locationProvider', '$sceDelegateProvider', 
     function($routeProvider, $locationProvider, $sceDelegateProvider) {
 	$routeProvider
@@ -18,18 +18,41 @@ angular.module('bupasearch', ['ngRoute', 'buparesults'])
 }])
 .controller("mainCtrl", ['$scope', '$location', 'Gsa',
 	function($scope, $location, Gsa) {
-    $scope.currentCollection = $location.search().site;
-    $scope.cards = {
-    	bankofengland: {
-    		title: 'Products',
-    		collection: 'bankofengland'
-    	},
-    	lloyds: {
-    		title: 'Health',
-    		collection: 'lloyds'
-    	}
+        $scope.params = $location.search();
+
+        $scope.searchbox = $scope.params.q;
+
+        var allCollection = 'all';
+        if($scope.params.site == allCollection){
+            $scope.currentView = 'cards';
+        }else{
+            $scope.currentView = 'fullscreen';
+        }
+
+        $scope.cards = {
+        	bankofengland: {
+        		title: 'Products',
+        		collection: 'bankofengland'
+        	},
+        	lloyds: {
+        		title: 'Health',
+        		collection: 'lloyds'
+        	}
+        }
+
+        //build hrefs for cards
+        var p = jQuery.extend({}, $location.search());
+        for(var i in $scope.cards){
+            var collection = $scope.cards[i].collection
+            p.site = collection;
+            $scope.cards[i].href = '/search?' + jQuery.param(p);
+        }
+
+        //build all href
+        p.site = allCollection;
+        $scope.allHref = '/search?' + jQuery.param(p);
     }
-}])
+])
 .filter('html', ['$sce', function($sce){
     return function(text) {
         return $sce.trustAsHtml(text);
