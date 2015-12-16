2686285f01744cc9dc265481f15d9cb4e111ee08
Peter Radcliffe <peter.radcliffe@cloudsherpas.com>
2015-12-16 13:09:54 +0000

    altered keymatch styling added minimum zoom to maps

--- bupa-angular/directives.js
+++ bupa-angular/directives.js
@@ -1,6 +1,6 @@
 angular.module('buparesults', ['bupasearchservice'])
-.directive('resultscard', ['Gsa', '$location', 'uiGmapGoogleMapApi',
-    function(Gsa, $location, uiGmapGoogleMapApi) {
+.directive('resultscard', ['Gsa', '$location', 'uiGmapGoogleMapApi', 'uiGmapIsReady',
+    function(Gsa, $location, uiGmapGoogleMapApi, uiGmapIsReady) {
   return {
     restrict: 'A',
     templateUrl: bupaconst.resourceuri + 'resultsTemplate.html',
@@ -33,6 +33,16 @@ angular.module('buparesults', ['bupasearchservice'])
                      zoom: 6,
                      pan: false
                  };//make a separate map for responsive use
+            });
+            //supply int of 2 as its the number of maps on the page
+            uiGmapIsReady.promise(2).then(function (maps) {
+            	//set the zoom on all maps to a minumum level if exceeded.
+            	for(var i in maps){
+            		var map = maps[i].map
+            		if(map.getZoom() > 15){
+            			map.setZoom(15)
+            		}
+            	}
             });
         }
     }
